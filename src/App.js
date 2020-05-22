104ac7dd0a2567ebb173304edaf204662ce549df
Julie Engel <julie.engel@mail.com>
2020-05-22 21:13:44 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const msg = "This is my message to you.";
+
+    const divStyle = {
+        color: 'blue'
+    };
+
+    const handleClick = () => {
+        alert("you clicked again");
+    }
+
+    return (
+        <div className="helloworld">
+            <h1>Hello, World</h1>
+            <h2>First React App</h2>
+            <p style={divStyle}>Second React App</p>
+            <p onClick={() => alert("you clicked...")}>{msg}</p>
+            <p onClick={handleClick}>{msg}</p>
+        </div>
+    );
 }
 
-export default App;
+export default App
